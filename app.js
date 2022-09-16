101fa51d8a5a65ae4a124c004406828737af846a
jricot01 <jricot01@gmail.com>
2022-09-16 15:29:28 -0400

    Completed

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 // TODO: Enable strict mode
+"use strict";
 
 // TODO: Fix the following parameter list
-function parseToJSON(data, data) {
-  // TODO: Add a try/catch block to
-  // attempt to convert 'data' to JSON
-  return JSON.parse(data); 
-  
+// TODO: Add a try/catch block to
+// attempt to convert 'data' to JSON
+function parseToJSON(data) {
+  try {
+    return JSON.parse(data);
+  } catch (error) {
+    console.error(error.message);
+    return null;
+  }
+
   // TODO: if an exception is raised
   // print the error to the console
   // and return null
 }
 
-let failData = "<h1>Hello World!</h1>"
+let failData = "<h1>Hello World!</h1>";
 
 let passData = JSON.stringify({ success: true });
 
-console.log(parseToJSON(failData));
+console.log(parseToJSON(failData)); // null
 
-console.log(parseToJSON(passData));
+console.log(parseToJSON(passData)); // null
